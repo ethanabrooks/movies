864d65e82578bdc140e9cca0e43071319fbffd0c
Ethan <ethanabrooks@gmail.com>
2016-03-26 13:05:48 -0400

    init

---
+++ main.py
@@ -0,0 +1,51 @@
+import argparse
+import model
+
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', action='store_true', help='train model from scratch')
+    parser.add_argument('-u', '--username', type=str, help='user to predict ratings for')
+    parser.add_argument('-m', '--movie', type=str, help='movie to predict ratings for')
+    args = parser.parse_args()
+
+    # check if user has entered both a username and a movie title
+    username_and_movie_warning = 'Warning: the model cannot make predictions ' \
+                                 'unless you enter both a username and a movie title.'
+    if args.username and not args.movie:
+        print(username_and_movie_warning)
+        args.movie = input('Please enter the movie you would like '
+                           'to predict %s\'s ratings for: ' % args.username)
+    if args.movie and not args.username:
+        print(username_and_movie_warning)
+        args.username = input('Please enter the user whose rating of %s '
+                              'you would like to predict: ' % args.movie)
+
+    # path to saved version of trained model
+    load_path = os.path.join('models', 'model.cpt')
+
+    # check if a model has been previously trained
+    already_trained = os.path.exists(load_path)
+    if not (args.train or already_trained):
+        response = input('Model has not been trained. '
+                         'Train it now (this may take several minutes)?')
+        while True:
+            if response in 'Yes yes':
+                args.train = True
+                break
+            elif response in 'No no':
+                print('Ok. Goodbye.')
+                exit(0)
+            else:
+                response = input('Please enter [y|n].')
+
+    if args.train:
+        model.train(load_path)
+
+    # predict a rating for the user
+    if args.username and args.movie:
+        model.load(load_path)
+        prediction = model.predict(args.username, args.movie)
+        print("The model predicts that %s will give %s a %d"
+              % args.username, args.movie, prediction)
